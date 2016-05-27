2c320f5421d2ab00307b2189f580474f95223c38
Ross McDonald <ross@influxdb.com>
2016-05-27 13:31:44 -0500

    Update docker entrypoints. Add opensuse dockerfiles.

--- docker/build.sh
+++ docker/build.sh
@@ -7,10 +7,14 @@ function err {
 }
 
 echo "Building CentOS containers..."
-docker build -f Dockerfile_centos_6 -t ansible.influxdb.centos.6:latest . || err "Could not build CentOS 6 container"
-docker build -f Dockerfile_centos_7 -t ansible.influxdb.centos.7:latest . || err "Could not build CentOS 6 container"
+docker build -f Dockerfile_centos_6 -t ansible:centos6 . || err "Could not build CentOS 6 container"
+docker build -f Dockerfile_centos_7 -t ansible:centos7 . || err "Could not build CentOS 6 container"
 
 echo "Building Ubuntu containers..."
-docker build -f Dockerfile_ubuntu_trusty -t ansible.influxdb.ubuntu.trusty:latest . || err "Could not build Ubuntu Trusty container"
-docker build -f Dockerfile_ubuntu_wily -t ansible.influxdb.ubuntu.wily:latest . || err "Could not build Ubuntu Wily container"
-docker build -f Dockerfile_ubuntu_xenial -t ansible.influxdb.ubuntu.xenial:latest . || err "Could not build Ubuntu Xenial container"
+docker build -f Dockerfile_ubuntu_trusty -t ansible:ubuntu_trusty . || err "Could not build Ubuntu Trusty container"
+docker build -f Dockerfile_ubuntu_wily -t ansible:ubuntu_wily . || err "Could not build Ubuntu Wily container"
+docker build -f Dockerfile_ubuntu_xenial -t ansible:ubuntu_xenial . || err "Could not build Ubuntu Xenial container"
+
+echo "Building Ubuntu containers..."
+docker build -f Dockerfile_opensuse_harlequin -t ansible:opensuse_harlequin . || err "Could not build OpenSuse Harlequin container"
+docker build -f Dockerfile_opensuse_leap -t ansible:opensuse_leap . || err "Could not build OpenSuse Leap container"
